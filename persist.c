32195c5217464a1d647f9da63321f7fe25780535
MatheusSilva020 <65418727+MatheusSilva020@users.noreply.github.com>
2020-12-03 00:43:07 -0300

    Update persist.c

--- persist.c
+++ persist.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include "persist.h"
 
+//saveALL: salva todos os registros do programa.
 void saveAll(){
     persistClient();
     persistStock();
     persistCard();
     persistSale();
 }
+
+//loadALL: carrega todos os registros do programa.
 void loadAll(){
     loadClient();
     loadStock();
