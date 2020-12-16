4b8100b60bd3d2f2db35e8eab377cf5ba59c1944
Wellington Abilio Alves Silverio <wellington.alves.silverio@gmail.com>
2020-12-15 22:44:56 -0300

    remove createShoppingCard

--- shopping_cart.c
+++ shopping_cart.c
@@ -2,11 +2,6 @@
 #include "sales.h"
 #include "stock.h"
 
-//createShoppingCard: recebe os dados do produto e cria um novo carrinho de compras a lista de carrinhos.
-void createShoppingCard(int saleCode, int amount){
-
-}
-
 //refund: recebe os dados do carrinho e realiza a troca(ressarcimento) do produto.
 void refound(int cardCode, int saleCode){
     Card* card = getCardByCode(cardCode);
