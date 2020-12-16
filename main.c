b37e32bf3f7c2425674f7e16af53a4753e6c0a74
MatheusSilva020 <65418727+MatheusSilva020@users.noreply.github.com>
2020-12-15 21:20:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,15 @@
   Matheus Eduardo da Silva  RA:230719
   Caio Pereira Masseu       RA:256341
   Wellington A. A. Silverio RA:178667
+  
+  PROJETO PROG II - PARTE 3
+  LOJA DE ARTIGOS ESPORTIVOS
 */
 
 #include <stdlib.h>
 #include "menu.h"
 
+//main: funcao principal.
 int main(){
 	menu();
 
